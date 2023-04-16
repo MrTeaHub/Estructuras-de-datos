94b1d024dbd0217277c4a0ab7094d90ae475cce6
Chivataj <chivataj@uninorte.edu.co>
2023-04-15 21:23:58 -0500

    add 4

--- lab3/4.cpp
+++ lab3/4.cpp
@@ -1,3 +1,16 @@
+/*
+En un acertijo el problema es encontrar la mayor cantidad de chocolates 
+que se pueda comer, siguiendo las recomendaciones : 
+a. dinero : Dinero que se tiene para comprar chocolates.
+b. precio : Precio de un chocolate.
+c. envoltura : Número de envolturas a devolver para recibir un chocolate  extra.
+Se puede suponer que todos los valores dados son números enteros 
+positivos y mayores que 1. 
+Un método consiste en contar continuamente la cantidad de chocolates 
+devolviendo las envolturas hasta que las envolturas que quedan no 
+alcancen para canjear un chocolate.
+*/
+
 #include <iostream>
 #include <vector>
 
